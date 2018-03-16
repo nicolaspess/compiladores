b2a49c6c83a226476f041ce15b88ff1ac2e7018b
nidejelski <nidejelski@gmail.com>
2018-03-16 16:36:02 -0300

    Update tabelaHash.c

--- Etapa1/tabelaHash.c
+++ Etapa1/tabelaHash.c
@@ -1,26 +1,27 @@
 // Etapa 1
 // Daniel Machado Nidejelski, Nícolas Vincent Dall'Bello Pessutto 
+#include "tabelaHash.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "tabelaHash.h"
 
-void hashInit(){
+void hash_Init(){
 	//implementar
 }
 
-void hashPrint(){
+void hash_Print(){
 	//implementar
 }
 
-int hashAddress(char* texto){
+hash_Node* hashFind(char* texto){
 	//implementar
 }
 
-hashNode* hashFind(char* texto){
+hash_Node* hashInsert(int tipo, char* texto){
 	//implementar
 }
 
-hashNode* hashInsert(int tipo, char* texto){
+int hash_Address(char* texto){
 	//implementar
 }
+
